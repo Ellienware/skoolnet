7959ccee0822aea17ad9e96332c9084113918f9a
Nelisa <139216476+Ellienware@users.noreply.github.com>
2025-04-20 23:03:47 +0200

    landing page complete

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -14,7 +14,9 @@ const eslintConfig = [
     extends: ['next'],
     rules: {
       'react/no-unescaped-entities': 'off',
+      "react/no-unescaped-entities": "off",
       '@next/next/no-page-custom-font': 'off',
+      '@typescript-eslint/no-unused-vars': "off"
     },
   }),
 ]
