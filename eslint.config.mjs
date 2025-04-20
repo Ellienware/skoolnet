472dd457f55176fff3784a870eef93cb4fb25393
Nelisa <139216476+Ellienware@users.noreply.github.com>
2025-04-20 23:11:45 +0200

    landing page complete

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -11,7 +11,7 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.config({
-    extends: ['next'],
+    extends: ['next/core-web-vitals'],
     rules: {
       'react/no-unescaped-entities': 'off',
       "react/no-unescaped-entities": "off",
