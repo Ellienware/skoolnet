6e4015e242026eada1f6dcb30bdeb9d8426e4dc1
Nelisa <139216476+Ellienware@users.noreply.github.com>
2025-04-21 05:40:53 +0200

    landing page complete

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -14,9 +14,9 @@ const eslintConfig = [
     extends: ['next/core-web-vitals'],
     rules: {
       'react/no-unescaped-entities': 'off',
-      "react/no-unescaped-entities": "off",
       '@next/next/no-page-custom-font': 'off',
-      '@typescript-eslint/no-unused-vars': "off"
+      '@typescript-eslint/no-unused-vars': "off",
+      "no-unused-vars": "off"
     },
   }),
 ]
